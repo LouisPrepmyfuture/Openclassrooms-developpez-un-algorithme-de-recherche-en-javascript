88d45181f1bb7233e886d47926013f26a56b2aad
LouisPrepmyfuture <97509071+LouisPrepmyfuture@users.noreply.github.com>
2023-01-17 21:02:45 +0100

    test

--- scripts/search.js
+++ scripts/search.js
@@ -1,23 +1,38 @@
+// recupère la list des tags toLowerCase
+function listKeyWord(recipes) {
+	const list_ingredients = []
+	const list_utensils = []
+	const list_appliances = []
+	let all_lists = {}
 
-// recupère les list des most clée
-function listKeyWord(data) {
-  const  list_ingredients = []
-  const  list_utensils = []
-  const  list_appliances = []
-  let    all_lists = {}
+	recipes.forEach(recipe => {
+		// list tag ingredient
+		recipe.ingredients.forEach(ingredient => {
+			list_ingredients.push(ingredient.ingredient.toLowerCase())
+		})
 
-  for(let i = 0; i < data.length; i++){
-    // list ingredient
-    const total = Object.keys(data[i].ingredients).length
-    for(let j = 0; j < total; j++){
-      list_ingredients.push(data[i].ingredients[j].ingredient)
-    }
-    return all_lists
+		// list tag ustensil appliance
+		recipe.ustensils.forEach(ustensil => {
+			list_utensils.push(ustensil.toLowerCase())
+		})
+
+		// list appliance tag 
+		list_appliances.push(recipe.appliance.toLowerCase())
+
+	})
+	// sup duplicate
+	all_lists = {
+		ingredient: sup_duplicate(list_ingredients),
+		ustensil: sup_duplicate(list_utensils),
+		appliance: sup_duplicate(list_appliances)
+	}
+
+	return all_lists
 }
 
-function filter_tag(tags,valeur){
-  let resulta = []
-  return resulta = tags.filter(tag => tag.includes(valeur.toLowerCase()))
+function filter_tag(tags, valeur) {
+	let resulta = []
+	return resulta = tags.filter(tag => tag.includes(valeur.toLowerCase()))
 }
 
 /**
@@ -26,74 +41,112 @@ function filter_tag(tags,valeur){
  * @param {Object} filterParams mainInput + tag 1 + tag 2 + tag 3
  * @return {Array} - resultat de data filtré
  */
- function searchData(recipes, filterParams) {
+function searchData(recipes, filterParams) {
 
-  const { input_value, list_tag_ingredient, list_tag_ustensil, list_tag_appliance} = filterParams;
-  
-  return recipes.filter((recipe) => {
-    // mes conditions pour garder l'élement
-    // TODO forcer la comparaison sans tenir compte de la casse
-    // on vérifie si le texte est présent dans la description ou 1 ingrédient ou le nom
-      
-    if (input_main(recipe,input_value) === false) {
-      return false;
-    } 
-   
-    if (list_tag_ingredient.length != 0) {
-      if(notInIngredients(list_tag_ingredient, recipe)){
-        return false
-      }
-    }  
+	const { input_value, list_tag_ingredient, list_tag_ustensil, list_tag_appliance } = filterParams;
 
-    if (list_tag_ustensil.length != 0) {
-      if(notInUstensils(list_tag_ustensil, recipe)){
-        return false
-      }
-    } 
+	return recipes.filter((recipe) => {
+		// mes conditions pour garder l'élement
+		// TODO forcer la comparaison sans tenir compte de la casse
+		// on vérifie si le texte est présent dans la description ou 1 ingrédient ou le nom
 
-    if (list_tag_appliance.length != 0) {
-      if (list_tag_appliance.length < 2) {
-        if(recipe.appliance.toLowerCase() != list_tag_appliance[0]){
-          return false
-        }
-      }else{
-        return false 
-      }
-    }   
-    // garder item si on retourne true
-    return true;
-  });
+		if (input_main(recipe, input_value) === false) {
+			return false;
+		}
+
+		if (list_tag_ingredient.length != 0) {
+			if (notInIngredients(list_tag_ingredient, recipe)) {
+				return false
+			}
+		}
+
+		if (list_tag_ustensil.length != 0) {
+			if (notInUstensils(list_tag_ustensil, recipe)) {
+				return false
+			}
+		}
+
+		if (list_tag_appliance.length != 0) {
+			if (list_tag_appliance.length < 2) {
+				if (recipe.appliance.toLowerCase() != list_tag_appliance[0]) {
+					return false
+				}
+			} else {
+				return false
+			}
+		}
+		// garder item si on retourne true
+		return true;
+	});
 }
 
-function input_main(recipe,input_value){
-  if( recipe.name.toLowerCase().indexOf(input_value) === -1
-  && recipe.description.toLowerCase().indexOf(input_value) === -1
-  && notInputInIngredients(input_value, recipe)
-  ){
-    return false
-  }else{
-    return true;
+function input_main(recipe, input_value) {
+	if (input_value === "") {
+		return true
 	}
+	// search in ingredient 
+	let ingredients = recipe.ingredients
+	for (let i = 0; ingredients.length > i; i++) {
+		let ingredient = ingredients[i]
+		if (ingredient.ingredient === input_value) {
+			return true
+		}
+	}
+	// search in description
+	if (recipe.name.toLowerCase().search(input_value) > -1) {
+		console.log("tresdt")
+		return true
+	}
+	// search in title
+	if (recipe.description.toLowerCase().search(input_value) > -1) {
+		return true
+	}
+
+	return false
 }
 
-searchRecipes(recipes, input_search, content_card)
 
-   let list_tag_ingredient = arrayPushString(".tag_ingredient")
-   let list_tag_ustensil = arrayPushString(".tag_ustensil")
-   let list_tag_appliance = arrayPushString(".tag_appliance")
-   let input_value = input_search.value.toLowerCase()
+function notInUstensils(list_tag_ustensil, recipe) {
+	let resulta = []
+	let recipe_ustensils_lowCase = recipe.ustensils.map(u => u.toLowerCase())
+	list_tag_ustensil.forEach(tag => {
+		resulta.push(recipe_ustensils_lowCase.includes(tag))
+	})
+	return resulta.some(c => c === false)
+}
 
-   let filterParams = { input_value, list_tag_ingredient, list_tag_ustensil, list_tag_appliance}
+function notInIngredients(list_tag_ingredient, recipe) {
+	let resulta = []
+	list_tag_ingredient.forEach(tag => {
+		resulta.push(recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(tag)))
+	})
+	return resulta.some(c => c === false)
+}
 
-   let resulta =  searchData(data, filterParams)
+function notInputInIngredients(mainInput, recipe) {
+	return !recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(mainInput));
+}
 
-  //filtre les tags qui reste dans la data restante 
-  let list_tag = listKeyWord(resulta)
 
-  // mise a jour des tags 
-  actualise_tag(list_tag.ingredient, domDropdown.ingredient,"ingredient")
-  actualise_tag(list_tag.ustensil, domDropdown.ustensil,"ustensil")
-  actualise_tag(list_tag.appliance, domDropdown.appliance,"appliance")
+function searchRecipes(data, input_search, content_resulta) {
+	// recuperation des tag active
 
-  create_list_cards(resulta, content_resulta)
+	let list_tag_ingredient = arrayPushString(".tag_ingredient")
+	let list_tag_ustensil = arrayPushString(".tag_ustensil")
+	let list_tag_appliance = arrayPushString(".tag_appliance")
+	let input_value = input_search.value.toLowerCase()
+
+	let filterParams = { input_value, list_tag_ingredient, list_tag_ustensil, list_tag_appliance }
+
+	let resulta = searchData(data, filterParams)
+
+	//filtre les tags qui reste dans la data restante 
+	let list_tag = listKeyWord(resulta)
+
+	// mise a jour des tags 
+	actualise_tag(list_tag.ingredient, domDropdown.ingredient, "ingredient")
+	actualise_tag(list_tag.ustensil, domDropdown.ustensil, "ustensil")
+	actualise_tag(list_tag.appliance, domDropdown.appliance, "appliance")
+
+	create_list_cards(resulta, content_resulta)
 }
