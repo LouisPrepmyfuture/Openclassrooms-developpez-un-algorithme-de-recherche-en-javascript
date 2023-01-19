653991c2d708b0213202175a9191bf8f112d22ff
LouisPrepmyfuture <97509071+LouisPrepmyfuture@users.noreply.github.com>
2023-01-19 12:10:44 +0100

    reparation code

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,49 +1,47 @@
 // supprime les doublons d'un tableau 
-function sup_duplicate(list){
-  
-  return  list.filter(key => 
-    list.includes(key) === false
-  )
+function sup_duplicate(list) {
+
+	return list.filter(key =>
+		list.includes(key) === false
+	)
 }
 
-function sup_duplicate(list){
-  let list_sort =[]
-  for(let l = 0; l < list.length; l++){
-    if(list_sort.includes(list[l]) === false){
-      list_sort.push(list[l])
-    }
-  }
-  return list_sort
+function sup_duplicate(list) {
+	let list_sort = []
+	for (let l = 0; l < list.length; l++) {
+		if (list_sort.includes(list[l]) === false) {
+			list_sort.push(list[l])
+		}
+	}
+	return list_sort
 }
 
-
-
-function sup_child(parent){
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
+function sup_child(parent) {
+	while (parent.firstChild) {
+		parent.removeChild(parent.firstChild);
+	}
 }
 
-function actualise_tag(new_tag,content_old_list,  type_tag){
-  sup_child(content_old_list.content_list)
-  create_dropdoown(new_tag, content_old_list, type_tag)
+function actualise_tag(new_tag, content_old_list, type_tag) {
+	sup_child(content_old_list.content_list)
+	create_dropdoown(new_tag, content_old_list, type_tag)
 }
 
 // renvoi un tableau de tag selon le tag recherche a l'aide de sa .class
-function arrayPushString(tag){
-  let tag_array = []
-  let list_tag = document.querySelectorAll(tag)
-  list_tag.forEach(tag => {
-    tag_array.push(tag.innerHTML)
-  });
-  return tag_array
+function arrayPushString(tag) {
+	let tag_array = []
+	let list_tag = document.querySelectorAll(tag)
+	list_tag.forEach(tag => {
+		tag_array.push(tag.innerText)
+	});
+	return tag_array
 }
 
- // event si je click sur un des ingredients mes cette ingredients 
- function addEventListDropdown(list_tag, type_tag){
-   list_tag.forEach(tag => {
-      tag.addEventListener("click", function(){
-       create_btn_search(this.textContent,  type_tag)
-      })
-   });
+// event si je click sur un des ingredients mes cette ingredients 
+function addEventListDropdown(list_tag, type_tag) {
+	list_tag.forEach(tag => {
+		tag.addEventListener("click", function () {
+			create_btn_search(this.textContent, type_tag)
+		})
+	});
 }
